258c0dd907771b042123e4657945a12a24e5085e
emil_ <emil.kushner.stud@mail.ru>
2023-12-06 19:02:46 +0700

    Получение JSON-данных от Flickr

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,6 +4,12 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+        "&api_key=49d95c546c313f4dc025f5ed50bcdfdc" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
