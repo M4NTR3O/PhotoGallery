a5d2c0deb1ecce97b0a3895c760fe8c9649c724a
emil_ <emil.kushner.stud@mail.ru>
2023-12-08 17:37:34 +0700

    Простое сохранение с помощью SharedPreferences

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -112,6 +112,15 @@ class PhotoGalleryFragment: Fragment() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_clear -> {
+                photoGalleryViewModel.fetchPhotos("")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     private class PhotoHolder(itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView)
     {
